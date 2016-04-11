f417459b7ffbccd5ca8d2a9d999dfca7dc7164d8
RivervdMeulen <River@Rivers-MacBook-Pro.local>
2016-04-11 11:19:06 +0200

    Git Ignore & Timer Script

---
+++ GameJam3/Assets/Scripts/CountDownTimer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CountDownTimer : MonoBehaviour
+{
+	[SerializeField] private Text _countDownText;
+	public float timerCount;
+
+	void Update ()
+	{
+		if (timerCount <= 0)
+		{
+			//Hier stopt de game.
+			Time.timeScale = 0;
+			_countDownText.text = "Time left: 00:00:00";
+		}
+
+		else
+		{
+			Countdown();
+		}
+	}
+
+	private void Countdown()
+	{
+		timerCount -= Time.deltaTime;
+
+		var minutes = timerCount / 60;
+		var seconds = timerCount % 60;
+		var fraction = (timerCount * 100) % 100;
+
+		_countDownText.text = string.Format("Time left: {0:00}:{1:00}:{2:00}", Mathf.Floor(minutes), seconds, fraction); 
+	}
+}
