6522da7dd7f5090f73e47937e2dec35d22ce0098
Rico Evers <ricoevers@live.nl>
2016-04-11 12:26:28 +0200

    Eind punt
    Script voor end punt gemaakt.

---
+++ GameJam3/Assets/Scripts/EndPoint.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EndPoint : MonoBehaviour
+{
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.CompareTag("player"))
+		{
+			SceneManager.LoadScene ("WinScreen");
+		}
+		if(other.CompareTag("AI"))
+		{
+			SceneManager.LoadScene ("LoseScreen");
+		}
+	}
+}
